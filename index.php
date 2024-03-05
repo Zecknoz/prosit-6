6ea870fce3438f15c1a7c6ceb406f75a1fb5e011
TheoC <theo.cvg@gmail.com>
2024-03-05 09:20:59 +0100

    Fin de prosit

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php
-
 $numPage = 0;
-
 
 function printSmiley(){
     global $numPage;
     for ($i=0; $i<256; $i++){
-        echo '<p class="">&#' . strval(128512 + $i + (256*$numPage)) . '<p>';
+        echo '<div>&#' . strval(128512 + $i + (256*$numPage)) . '</div>';
     }
 };
 
 function getNextPage($to){
     global $numPage;
     if($to === 'minus'){
-        return $numPage + 0;
+        
+        return $_GET['page'] - 1;
     }
     if($to === 'plus'){
         return $numPage + 1;
@@ -22,14 +21,7 @@ function getNextPage($to){
         return 0;
     }
 }
-
-function updatePage(){
-    global $numPage;
-    return 'Page ' . strval($numPage);
-}
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -48,30 +40,33 @@ function updatePage(){
     </head>
     <main>
         <div class="flex justify-center">
-            <div class="my-8 flex flex-row space-x-4 place-items-center grow-0 shrink-0 basis-96">
-                <a href=<?="index.php?page=" . getNextPage('minus')?> class="flex bg-gray-200 px-2 rounded-md text-gray-600 items-center text-3xl h-1/4"><</a>
+            <div class="my-4 flex flex-row space-x-4 place-items-center">
+                <a href=<?="index.php?page=" . getNextPage('minus')?> class="flex bg-gray-200 px-2 rounded-md text-gray-600 items-center text-3xl h-1/4 hover:bg-gray-300 hover:text-white"><</a>
 
                 <div class="flex flex-col ">
                     <div class="flex justify-center">
                         <?php 
-                            echo(updatePage()) ?>
+                        if(isset($_GET['page'])){
+                            echo('Page ' . strval($_GET['page']));
+                        }else{
+                            echo 'Page 0';
+                        }
+                        ?>
                     </div>
-                    <div class="grid grid-cols-16 gap-2 bg-gray-200 rounded-lg">
+                    <div class="grid grid-cols-16 gap-3 bg-gray-200 rounded-lg">
                         <?php          
-
                             global $numPage;
-
                             if(isset($_GET['page'])){
                                 $numPage = $_GET['page'];
                                 printSmiley();
                             }else{
+                                $numPage = 0;
                                 printSmiley();
                             }
                         ?>
                     </div>
-                </div>
-                
-                <a href=<?="index.php?page=" . getNextPage('plus')?> class="flex bg-gray-200 px-2 rounded-md  text-gray-600 items-center text-3xl h-1/4">></a>
+                </div>  
+                <a href=<?="index.php?page=" . getNextPage('plus')?> class="flex bg-gray-200 px-2 rounded-md  text-gray-600 items-center text-3xl h-1/4 hover:bg-gray-300 hover:text-white">></a>
             </div>
         </div>
     </main>
